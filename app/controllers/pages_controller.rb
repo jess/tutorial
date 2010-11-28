08dd6260502543e0753a73a599d8e5a5d364c203
Jess Brown <jess@brownwebdesign.com>
2010-11-28 18:06:29 -0500

    added static pages and tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @page_title = "Home"
+  end
+  
+  def about
+    @page_title = "About"
   end
 
   def contact
+    @page_title = "Contact"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
-
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should contain title Home" do
+      get 'home'
+      response.should have_selector("title", :content => "Home")
     end
   end
 
@@ -13,6 +17,21 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    it "should contain title contact" do
+      get 'contact'
+      response.should have_selector("title", :content => "Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should contain title About" do
+      get 'about'
+      response.should have_selector("title", :content => "About")
     end
   end
 
